e14ce6cd768504720c42e2587fbd6e6eb6da222e
pidokige02 <pidokige0204@gmail.com>
2023-03-04 11:56:23 +0900

    6. read

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import random
 
 app = Flask(__name__)
 
@@ -9,34 +8,44 @@ topics = [
     {'id': 3, 'title': 'javascruot', 'body': 'javascript is ...'}
 ]
 
-@app.route('/')
-def index():
-    liTags = ''
-    for topic in topics:
-        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+# 유지보수의 편의성을 위해서 기본 HTML 코드를 템플릿화했습니다.
+def template(contents, content):
     return f'''<!doctype html>
     <html>
         <body>
             <h1><a href="/">WEB</a></h1>
             <ol>
-                {liTags}
+                {contents}
             </ol>
-            <h2>Welcome</h2>
-            Hello, Web
+            {content}
         </body>
     </html>
     '''
 
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
+
+@app.route('/')
+def index():
+    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
+
+@app.route('/read/<int:id>/')  # variable한 정보를 보낼경우
+def read(id):
+    title = ''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+    return template(getContents(), f'<h2>{title}</h2>{body}')
 
 @app.route('/create/')
 def create():
     return 'Create'
-
-
-@app.route('/read/<int:id>/')  # variable한 정보를 보낼경우
-def read(id):
-    print(id)
-    return 'Read '+str(id)
 
 app.run(debug=True)  # debug=True 는 code 변경시 자동으로 변경되어 실행됨
 # app.run(port=5001, debug=True)  # port 변경이 가능함
