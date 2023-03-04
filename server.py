da9b060c0be38acebd73c4ae23f28995275fb51d
pidokige02 <pidokige0204@gmail.com>
2023-03-04 10:38:50 +0900

    4.Routing

--- server.py
+++ server.py
@@ -6,8 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'random : <strong>'+str(random.random())
+    return 'Welcome'
 
+
+@app.route('/create/')
+def create():
+    return 'Create'
+
+
+@app.route('/read/<int:id>/')  # variable한 정보를 보낼경우
+def read(id):
+    print(id)
+    return 'Read '+str(id)
 
 app.run(debug=True)  # debug=True 는 code 변경시 자동으로 변경되어 실행됨
 # app.run(port=5001, debug=True)  # port 변경이 가능함
