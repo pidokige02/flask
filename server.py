fa46e0056c66ac356369ef40b460f3c8460c3913
pidokige02 <pidokige0204@gmail.com>
2023-03-04 10:42:44 +0900

    5. Homepage creation

--- server.py
+++ server.py
@@ -3,10 +3,29 @@ import random
 
 app = Flask(__name__)
 
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascruot', 'body': 'javascript is ...'}
+]
 
 @app.route('/')
 def index():
-    return 'Welcome'
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {liTags}
+            </ol>
+            <h2>Welcome</h2>
+            Hello, Web
+        </body>
+    </html>
+    '''
 
 
 @app.route('/create/')
